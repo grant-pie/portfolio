fc3b75cc90bfb5f3a77bc8b5a23628493900242b
grant-pie <grant.pieterse1@gmail.com>
2026-03-20 15:04:48 +0200

    Added favicon to head tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: { lang: 'en' },
       title: 'Grant Pieterse — Front-End Developer',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ],
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
