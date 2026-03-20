b877c5c5988102c72343451f9ae57eea330f157b
grant-pie <grant.pieterse1@gmail.com>
2026-03-20 15:44:18 +0200

    Added more favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,11 @@ export default defineNuxtConfig({
       htmlAttrs: { lang: 'en' },
       title: 'Grant Pieterse — Front-End Developer',
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
       ],
       meta: [
         { charset: 'utf-8' },
